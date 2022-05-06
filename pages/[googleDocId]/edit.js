d7afd94003498d52d12c2ee9d5832085ec3f0d7d
Xavier Damman <xdamman@gmail.com>
2022-05-06 11:25:08 +0200

    fix for /edit

--- pages/[googleDocId]/edit.js
+++ pages/[googleDocId]/edit.js
@@ -1,38 +1,7 @@
 import { getPageMetadata } from "../../lib/lib";
 import sitemap from "../../sitemap.json";
 
-export async function getStaticPaths() {
-  const paths = [];
-  Object.keys(sitemap).forEach((key) => {
-    if (key.match(/^collectives/)) return;
-    paths.push({
-      params: {
-        googleDocId: sitemap[key].googleDocId,
-      },
-    });
-    if (sitemap[key].aliases) {
-      sitemap[key].aliases.map((alias) => {
-        paths.push({
-          params: {
-            googleDocId: alias,
-          },
-        });
-      });
-    }
-    paths.push({
-      params: {
-        googleDocId: key,
-      },
-    });
-  });
-  // console.log(paths);
-  return {
-    paths,
-    fallback: true,
-  };
-}
-
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   const pageInfo = getPageMetadata(params.googleDocId);
   const googleDocId = pageInfo.googleDocId || params.googleDocId;
 
